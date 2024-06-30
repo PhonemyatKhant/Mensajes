6fb534250853a7aec447a0909ab71ca00136a565
phonemyatkhant <phonemyatkhant45@gmail.com>
2024-06-30 13:40:22 +0630

    conversation body setup

---
+++ app/api/messages/route.ts
@@ -0,0 +1,66 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+  try {
+    const currentUser = await getCurrentUser();
+    const body = await req.json();
+
+    const { message, image, conversationId } = body;
+
+    // IF NO CURRENT USER RETURN ERROR
+    if (!currentUser?.id || !currentUser?.email)
+      return new NextResponse("UNAUTHORIZE USER", { status: 500 });
+
+    const newMessage = await prisma.message.create({
+      include: {
+        seen: true,
+        sender: true,
+      },
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: { id: conversationId },
+        },
+        sender: {
+          connect: { id: currentUser.id },
+        },
+        seen: {
+          connect: {
+            id: currentUser.id,
+          },
+        },
+      },
+    });
+
+    const updatedConversation = await prisma.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    });
+
+
+    return NextResponse.json(newMessage);
+  } catch (error) {
+    console.log(error, "ERROR MESSAGE");
+    return new NextResponse("MESSAGE SENT ERROR", { status: 500 });
+  }
+}
