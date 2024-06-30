80dd5cffecf4a424f3d34d1ecbfdf138c5c0f778
phonemyatkhant <phonemyatkhant45@gmail.com>
2024-06-30 15:41:09 +0630

    finish cloudinary image upload functionality

--- components/ConversationBody.tsx
+++ components/ConversationBody.tsx
@@ -1,8 +1,46 @@
-import React from "react";
+"use client";
 
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
-const ConversationBody = () => {
-  return <div className=" flex-1 overflow-y-auto">ConversationBody</div>;
+import useConversation from "@/app/hooks/useConversation";
+// import { find } from "lodash";
+
+// import { pusherClient, pusherEvents } from "../../../libs/pusher";
+import { FullMessageType } from "@/types";
+import MessageBox from "./MessageBox";
+
+interface ConversationBodyProps {
+  initialMessages: FullMessageType[];
+}
+
+const ConversationBody: React.FC<ConversationBodyProps> = ({
+  initialMessages = [],
+}) => {
+  // BOTTOM SPACE
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  const [messages, setMessages] = useState(initialMessages);
+
+  const { conversationId } = useConversation();
+
+  // ON PAGE LOAD UPDATE SEEN MESSAGES
+  useEffect(() => {
+    axios.post(`/api/conversations/${conversationId}/seen`);
+  }, [conversationId]);
+
+  return (
+    <div className="flex-1 overflow-y-auto">
+      {messages.map((message, i) => (
+        <MessageBox
+          isLast={i === messages.length - 1}
+          key={message.id}
+          data={message}
+        />
+      ))}
+      <div className="pt-1" ref={bottomRef} />
+    </div>
+  );
 };
 
 export default ConversationBody;
---
+++ components/MessageBox.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import clsx from "clsx";
+import { useState } from "react";
+
+import { format } from "date-fns";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+import Avatar from "./Avatar";
+import { FullMessageType } from "@/types";
+// import ImageModal from "./ImageModal";
+
+interface MessageBoxProps {
+  data: FullMessageType;
+  isLast?: boolean;
+}
+
+// GOT PASSED IN MESSAGE DATA
+const MessageBox: React.FC<MessageBoxProps> = ({ data, isLast }) => {
+  const session = useSession();
+  const [imageModalOpen, setImageModalOpen] = useState(false);
+
+  //   CHECK IF THE SENDER OF THIS MESSAGE IS THE CURRENT USER
+  const isOwn = session.data?.user?.email === data?.sender?.email;
+
+  //   FROM SEENLIST:USER[] EXCLUDE THE SENDER, MAP THE REMAINING OTHER USER/S AND JOIN THEM WITH COMMA
+  const seenList = (data.seen || [])
+    .filter((user) => user.email !== data?.sender?.email)
+    .map((user) => user.name)
+    .join(", ");
+
+  const container = clsx("flex gap-3 p-4", isOwn && "justify-end");
+
+  //   IF OWN MESSAGE ORDER 2 MOVE RIGHT AND HIDE
+  const avatar = clsx(isOwn && "order-2");
+  const body = clsx("flex flex-col gap-2", isOwn && "items-end");
+  const message = clsx(
+    "text-sm w-fit overflow-hidden",
+    isOwn ? "bg-primary text-white" : "bg-gray-100  dark:bg-lightgray",
+
+    // IMAGE STYLE
+    data.image ? "rounded-md p-0" : "rounded-full py-2 px-3"
+  );
+
+  return (
+    <div className={container}>
+      {!isOwn && (
+        <div className={avatar}>
+          <Avatar user={data.sender} />
+        </div>
+      )}
+      <div className={body}>
+        {/* NAME DATE CONTAINER  */}
+        <div className="flex items-center gap-1">
+          {/* IF NOT OWN MESSAGE SHOW SENDER ON TOP  */}
+          {!isOwn && (
+            <div className="text-sm text-gray-500">{data.sender.name}</div>
+          )}
+
+          {/* DATE FORMAT DISPLAY  */}
+          <div className="text-xs text-gray-400">
+            {format(new Date(data.createdAt), "p")}
+          </div>
+        </div>
+
+        {/* MESSAGE CONTAINER  */}
+        <div className={message}>
+          {/* IMAGE MODEL POP UP  */}
+          {/* <ImageModal
+            src={data.image}
+            isOpen={imageModalOpen}
+            onClose={() => setImageModalOpen(false)}
+          /> */}
+
+          {/* IF IMAGE SHOW IMAGE ELSE SHOW MESSAGE  */}
+          {data.image ? (
+            <Image
+              alt="Image"
+              height="288"
+              width="288"
+              onClick={() => setImageModalOpen(true)}
+              src={data.image}
+              className="
+                object-cover 
+                cursor-pointer 
+                hover:scale-110 
+                transition
+              "
+            />
+          ) : (
+            <div>{data.body}</div>
+          )}
+        </div>
+
+        {/* IF OWN MESSAGE'S SEEN LIST IS NOT EMPTY SHOW THE SEEN USER/S */}
+        {isLast && isOwn && seenList.length > 0 && (
+          <div
+            className="
+            text-xs 
+            font-light 
+            text-gray-500
+            "
+          >
+            {`Seen by ${seenList}`}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MessageBox;
