39d553752430fea619a75762b0a01fdb7c0f4d64
phonemyatkhant <phonemyatkhant45@gmail.com>
2024-07-02 19:09:15 +0630

    fix production error

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        swcPlugins: [
-            ["next-superjson-plugin", {}]
-        ]
-    },
+    // experimental: {
+    //     swcPlugins: [
+    //         ["next-superjson-plugin", {}]
+    //     ]
+    // },
     images: {
         domains: ["res.cloudinary.com", "avatars.githubusercontent.com", "lh3.googleusercontent.com"],
     },
